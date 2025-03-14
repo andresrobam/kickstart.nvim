32b056c618938fc285e5c92edb0ac6cbcc6cb509
Andres Robam <andres.robam@1ot.mobi>
2025-03-14 16:21:19 +0200

    config changes

--- init.lua
+++ init.lua
@@ -1021,6 +1021,7 @@ require('lazy').setup({
 })
 
 require('nvim-treesitter.install').compilers = { 'clang' }
+vim.wo.relativenumber = true
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
